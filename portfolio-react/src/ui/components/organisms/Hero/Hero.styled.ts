d80be69738427d844d6de43af4afb4dbaff4622c
Santiago Benito <bmsanti@hotmail.com>
2024-10-16 16:12:28 +0200

    changed hero

---
+++ portfolio-react/src/ui/components/organisms/Hero/Hero.styled.ts
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: ${({ theme }) => theme.spacing.xl} ${({ theme }) => theme.spacing.xl};
+  background-image: url("src/assets/header-background.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+
+  ${({ theme }) => theme.mediaQueries.desktop} {
+    padding: ${({ theme }) => theme.spacing.xxxxl}
+      ${({ theme }) => theme.spacing.xxxxxl}
+      ${({ theme }) => theme.spacing.none}
+      ${({ theme }) => theme.spacing.xxxxxl};
+  }
+`;
+
+export const Image = styled.img`
+  margin-top: ${({ theme }) => theme.margin.xl};
+  width: 400px;
+  height: auto;
+  margin-bottom: -50px;
+
+  ${({ theme }) => theme.mediaQueries.desktop} {
+    width: 500px;
+  }
+`;
+
+export const TitleWrapper = styled.div`
+  width: 80%;
+  padding: ${({ theme }) => theme.spacing.none}
+    ${({ theme }) => theme.spacing.l};
+
+  ${({ theme }) => theme.mediaQueries.desktop} {
+    width: 60%;
+    padding: ${({ theme }) => theme.spacing.none}
+      ${({ theme }) => theme.spacing.xxxxxl};
+  }
+`;
+
+export const TextWrapper = styled.div`
+  margin: ${({ theme }) => theme.margin.xl};
+
+  ${({ theme }) => theme.mediaQueries.desktop} {
+    width: 45%;
+    padding: ${({ theme }) => theme.spacing.s}
+      ${({ theme }) => theme.spacing.xxxxxl};
+  }
+`;
+
+export const CtaWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  font-weight: 500;
+  margin-bottom: ${({ theme }) => theme.margin.xl};
+  gap: ${({ theme }) => theme.spacing.l};
+
+  ${({ theme }) => theme.mediaQueries.desktop} {
+    margin-bottom: ${({ theme }) => theme.margin.xxxl};
+    gap: ${({ theme }) => theme.spacing.xl};
+  }
+`;
---
+++ portfolio-react/src/ui/components/organisms/Navbar/Navbar.styled.ts
@@ -0,0 +1,121 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.nav<{ $isMenuOpen: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: ${({ theme }) => theme.spacing.xl}
+    ${({ theme }) => theme.spacing.xxl};
+  position: absolute;
+  z-index: 100;
+  background: rgba(0, 0, 0, 0.5);
+  overflow: ${({ $isMenuOpen }) => ($isMenuOpen ? "hidden" : "auto")};
+  transition: padding 0.3s ease;
+
+  ${({ theme }) => theme.mediaQueries.mobileAndTablet} {
+    padding: ${({ theme }) => theme.margin.l} ${({ theme }) => theme.margin.l};
+    align-items: flex-start;
+    gap: 10px;
+  }
+`;
+
+export const Logo = styled.img`
+  width: 10rem;
+`;
+
+export const Menu = styled.div<{ $isOpen: boolean }>`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  gap: ${({ theme }) => theme.spacing.xxl};
+  font-size: ${({ theme }) => theme.fontSize.l};
+  transition: transform 0.3s ease;
+
+  ul {
+    width: 100%;
+
+    ${({ theme }) => theme.mediaQueries.desktop} {
+      display: flex;
+      gap: ${({ theme }) => theme.spacing.xxl};
+    }
+  }
+
+  ${({ theme }) => theme.mediaQueries.mobileAndTablet} {
+    position: fixed;
+    flex-direction: column;
+    top: 0;
+    right: 0;
+    background-color: ${({ theme }) => theme.color.darkPurple};
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    transform: ${({ $isOpen }) =>
+      $isOpen ? "translateX(0)" : "translateX(100%)"};
+    padding-top: 70px;
+    overflow-y: auto;
+  }
+`;
+
+export const MenuItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacing.xxs};
+  cursor: pointer;
+  transition:
+    color 0.3s ease,
+    font-size 0.3s ease,
+    transform 0.3s ease;
+
+  &:hover {
+    color: ${({ theme }) => theme.color.purple};
+    transform: scale(1.15);
+    font-size: ${({ theme }) => theme.fontSize.xl};
+  }
+
+  ${({ theme }) => theme.mediaQueries.mobileAndTablet} {
+    font-size: 20px;
+    padding: 10px 0;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.spacing.xxs};
+
+    &:hover {
+      color: ${({ theme }) => theme.color.secondary};
+    }
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  ${({ theme }) => theme.mediaQueries.mobileAndTablet} {
+    display: none;
+  }
+`;
+
+export const HamburgerIcon = styled.img<{ $isOpen: boolean }>`
+  display: none;
+  cursor: pointer;
+
+  ${({ theme }) => theme.mediaQueries.mobileAndTablet} {
+    display: ${({ $isOpen }) => ($isOpen ? "none" : "block")};
+    top: 20px;
+    right: 25px;
+    z-index: 3;
+  }
+`;
+
+export const CloseIcon = styled.img`
+  display: none;
+  cursor: pointer;
+
+  ${({ theme }) => theme.mediaQueries.mobileAndTablet} {
+    display: block;
+    position: absolute;
+    top: 20px;
+    right: 30px;
+    width: 30px;
+    z-index: 2;
+  }
+`;
